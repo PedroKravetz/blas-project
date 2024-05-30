3f35967f31befd21d5cb4b27f4d606039671e66f
PedroKravetz <p.v.kravetz@gmail.com>
2024-05-30 18:14:06 -0300

    Python em API e inicio do Node

--- python/main.py
+++ python/main.py
@@ -1,9 +1,17 @@
 import numpy as np
 import pandas as pd
 import os
+import io
+import time
 from PIL import Image as im 
+from flask import Flask, request, jsonify
+from io import StringIO
 
 current_directory = os.getcwd()
+app = Flask(__name__)
+h1 = np.array(pd.read_csv(current_directory+'\\h1.csv', header=None, delimiter=','))
+h2 = np.array(pd.read_csv(current_directory+'\\h2.csv', header=None, delimiter=','))
+g1 = np.array(pd.read_csv(current_directory+'\\G-1.csv', header=None, delimiter=';'))
 
 def normalize(l):
     c=0
@@ -14,11 +22,13 @@ def normalize(l):
     return c
 
 def cgnr(g, h):
+    i = 0
     f0 = 0
     r0 = g
     z0 = h.transpose().dot(r0)
     p0=z0
     while (1):
+        i+=1
         w=h.dot(p0)
         nz0 = normalize(z0)
         nw = normalize(w)
@@ -35,14 +45,16 @@ def cgnr(g, h):
             array2 = np.reshape(f1, (60, 60)).transpose()
             data2 = im.fromarray((abs(array2*255)).astype(np.uint8))
             data2.save('teste.png') 
-            return f1
+            return [data1, i]
         r0=r1
 
 def cgne(g, h):
     f0 = 0
     r0 = g
     p0 = h.transpose().dot(r0)
+    i=0
     while (1):
+        i+=1
         a0 = (r0.transpose().dot(r0))/(p0.transpose().dot(p0))
         f1 = f0 + a0*p0
         r1 = r0 - a0*h.dot(p0)
@@ -52,7 +64,7 @@ def cgne(g, h):
             array2 = np.reshape(f1, (60, 60)).transpose()
             data2 = im.fromarray((abs(array2*255)).astype(np.uint8))
             data2.save('teste2.png') 
-            return f1
+            return [data2, i]
         p0 = p1
         r0 = r1
         f0 = f1
@@ -67,50 +79,67 @@ def ganhoSinal1(matriz):
                     matriz[x-1][y-1]*=ganho1
     return matriz
 
+def ganhoSinal2(matriz):
+    ganho2 = 0
+    for x in range(65):
+        if (x):
+            for y in range(437):
+                if (y):
+                    ganho2 = 100 + 0.05*y*np.sqrt(y)
+                    matriz[x-1][y-1]*=ganho2
+    return matriz
 
-h1 = np.array(pd.read_csv(current_directory+'\\h1.csv', header=None, delimiter=','))
-h2 = np.array(pd.read_csv(current_directory+'\\h2.csv', header=None, delimiter=','))
-g1 = np.array(pd.read_csv(current_directory+'\\G-1.csv', header=None, delimiter=';'))
-g2 = np.array(pd.read_csv(current_directory+'\\A-30x30-1.csv', header=None, delimiter=';'))
-aux = h1.transpose().dot(h1).transpose()
-c1 = normalize(aux)
-regularizacao1=-1
-aux = h1.transpose().dot(g1)
-for j in aux:
-    for i in j:
-        if (abs(i)>regularizacao1):
-            regularizacao1=abs(i)
-regularizacao1*=0.1
+def regularizacao(matriz):
+    regularizacao1=-1
+    aux = h1.transpose().dot(g1)
+    for j in aux:
+        for i in j:
+            if (abs(i)>regularizacao1):
+                regularizacao1=abs(i)
+    regularizacao1*=0.1
+    return regularizacao1
 
+def reducao(matriz):
+    return normalize(matriz.transpose().dot(matriz))
 
+@app.post("/blas")
+def control():
+    file = request.files.get("sinal")
+    file = file.read()
+    s=str(file,'utf-8')
+    data = StringIO(s) 
+    matriz = np.array(pd.read_csv(data, header=None, delimiter=','))
+    usuario = request.values["usuario"]
+    modelo = int(request.values["modelo"])
+    print(usuario)
+    print(modelo)
+    print(g1)
+    print(matriz)
+    inicio = 0
+    fim =  0
+    if (modelo == 1):
+        inicio= time.time()
+        lista = cgnr(matriz, h1)
+    else:
+        inicio= time.time()
+        lista = cgnr(matriz, h2)
+    fim=time.time()-inicio
+    return {"sinal": lista[0].tolist(), "tempo": fim, "usuario": usuario, "interacoes": lista[1]}, 200
+    #return {"retorno": "verdadeiro"}, 200
 
+@app.route("/")
+def hello_world():
+    return "<p> Hello World!</p>"
 
-
-aux = h2.transpose().dot(h2).transpose()
-c2 = normalize(aux)
-regularizacao2=-1
-aux = h2.transpose().dot(g2)
-for j in aux:
-    for i in j:
-        if (abs(i)>regularizacao2):
-            regularizacao2=abs(i)
-regularizacao2*=0.1
-ganho2 = 0
-for x in range(65):
-    if (x):
-        for y in range(437):
-            if (y):
-                ganho2 = 100 + 0.05*y*np.sqrt(y)
-
-print(h1)
-print(g1)
-print(c1)
-print(regularizacao1)
-print(h2)
-print(g2)
-print(c2)
-print(regularizacao2)
-print(ganho2)
-print(cgnr(g1, h1))
-print(cgne(g1, h1))
+#print(h1)
+#print(g1)
+#print(c1)
+#print(regularizacao1)
+#print(h2)
+#print(g2)
+#print(c2)
+#print(regularizacao2)
+#print(ganho2)
+#print(cgnr(g1, h1))
+#print(cgne(g1, h1))
 #print(cgnr(g2, h2))
