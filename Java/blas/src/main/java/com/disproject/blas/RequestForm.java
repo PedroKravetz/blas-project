d4a38b29ac56f1f234b563d106ce82badad04e03
PedroKravetz <p.v.kravetz@gmail.com>
2024-06-09 19:31:06 -0300

    Aplicação Java Funcionando

--- Java/blas/src/main/java/com/disproject/blas/RequestForm.java
+++ Java/blas/src/main/java/com/disproject/blas/RequestForm.java
@@ -2,20 +2,20 @@ package com.disproject.blas;
 
 public class RequestForm {
     private String usuario;
-    private float[] sinal;
+    private float[][] sinal;
     private int modelo;
 
-    public RequestForm(String user, float[] sig, int model){
+    public RequestForm(String user, float[][] sig, int model){
         this.usuario = user;
         this.sinal = sig;
         this.modelo = model;
     }
 
     public void setUsuario(String user) {this.usuario = user;}
-    public void setSinal(float[] sig) {this.sinal = sig;}
+    public void setSinal(float[][] sig) {this.sinal = sig;}
     public void setModelo(int model) {this.modelo = model;}
     
     public String getUsuario() {return this.usuario;}
-    public float[] getSinal() {return this.sinal;}
+    public float[][] getSinal() {return this.sinal;}
     public int getModelo() {return this.modelo;}
 }
